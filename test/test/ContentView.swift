29a558902f773e202f93bb6a405845490b3aae2d
hyperionSource <73002426+hyperionSource@users.noreply.github.com>
2020-10-17 12:37:05 +0200

    Add files via upload

---
+++ test/test/ContentView.swift
@@ -0,0 +1,28 @@
+//
+//  ContentView.swift
+//  test
+//
+//  Created by Sébastien - macbook pro on 15/10/2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+        if (true) {
+        }else {
+            
+        }
+    }
+}
+
+
+
---
+++ test/test/testApp.swift
@@ -0,0 +1,20 @@
+//
+//  testApp.swift
+//  test
+//
+//  Created by Sébastien - macbook pro on 15/10/2020.
+//
+
+import SwiftUI
+
+@main
+struct testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
+
+
+// test v
